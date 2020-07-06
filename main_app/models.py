5aaa98b06e72ece20027af5e5dd1fdb9946fac3f
xscullark <xscullark@gmail.com>
2020-07-06 17:11:11 -0400

    models/ sign up form

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+        email = models.EmailField(max_length=50)
+        first_name = models.CharField(max_length=50)
+        last_name = models.CharField(max_length=50)
+
+
+

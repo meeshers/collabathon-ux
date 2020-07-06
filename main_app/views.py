3681e1d45c867603811f22a2e913a5293afc87f2
Michelle Tran <m.tran1223@gmail.com>
2020-07-06 14:11:55 -0700

    routes working

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def info(request):
+  return render(request, 'info.html')
